f29dc600a925f9d75c0fd13ce6f0f6e2b3c69bfa
niezhengwei <11624179272@qq.com>
2018-03-17 10:42:06 +0800

    leon

---
+++ Spring/src/Test1/java/Test20180315.java
@@ -0,0 +1,17 @@
+import Service.IAccountService;
+import Service.StockExcpetion;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by leon on 2018/3/15.
+ */
+public class Test20180315 {
+    @Test
+    public void Test01() throws StockExcpetion {
+        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContextTrancation.xml");
+        IAccountService accountService = (IAccountService)context.getBean("accountService");
+        accountService.updateService(1,500,1,20);
+    }
+}
